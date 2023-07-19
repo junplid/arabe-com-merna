459a0768fbe4dfbd87808597ec5c73303984f41e
ousadojunplid <ousadojunplid@gmail.com>
2023-07-19 19:46:37 -0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      maxWidth: {
+        "default": "73.125rem"
+      },
+      screens: {
+        mobile: "500px"
+      },
+      colors: {
+        "light-gray": "#F6F6DE",
+        primary: "#075744",
+        white: "#F0F0D8",
+        "secondary": "#89C030"
+      },
+      backgroundColor: {
+        "primary": "#0C6746",
+        "primary-hover": "#0c7850",
+        "secondary": "#89C030"
+      }
+    },
+  },
+  plugins: [],
+}
