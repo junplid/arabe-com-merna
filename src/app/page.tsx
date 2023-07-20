3f86e807beeed890a5d610a4bb5437705b84490e
ousadojunplid <ousadojunplid@gmail.com>
2023-07-20 11:35:03 -0300

    up main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>asas</div>;
+  return (
+    <div className="h-28 text-primary font-semibold w-full flex justify-center items-center">
+      Main Content
+    </div>
+  );
 }
